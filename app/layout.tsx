149abcdd1c4c663caf1b457bd1f4967f6e894418
purpl3bot <mechi.farias@gmail.com>
2024-09-21 13:39:09 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Spectral } from 'next/font/google'
+import { Astloch } from 'next/font/google'
+
+const spectral = Spectral({ subsets: ['latin'], weight: ['400', '700'], variable: '--font-spectral' })
+const astloch = Astloch ({ variable: '--font-astlock' })
+
+export const metadata = {
+  title: 'Mercedes Farias',
+  description: 'Portfolio ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={`${spectral.variable} ${astloch.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
