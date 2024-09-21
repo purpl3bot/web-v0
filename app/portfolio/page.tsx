07f0a510b719eb279e4357d74c0d2e9241bd051a
purpl3bot <mechi.farias@gmail.com>
2024-09-21 17:40:36 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from 'react'
 import Layout from '../../components/Layout'
-import Pdf from '@/public'
+import Pdf from '../public'
 
     <Layout>
       <div>
+          /* <!-- <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe> --> */ 
+        /*  <embed src="Pdf" width="800px" height="2100px" /> */ 
         <iframe src={Pdf} width="100%" height="600px" style={{ border: 'none' }}></iframe>  
       </div>
     </Layout>
