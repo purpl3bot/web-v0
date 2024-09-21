aa8aac978d7c279844ffa57bd4a396bfc843d4d2
purpl3bot <mechi.farias@gmail.com>
2024-09-21 17:23:00 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,25 +2,13 @@
 
 import { useState } from 'react'
 import Layout from '../../components/Layout'
-
-export default function Portfolio() {
-  const [pdfUrl, setPdfUrl] = useState<string | null>(null)
-
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0]
-    if (file) {
-      const url = URL.createObjectURL(file)
-      setPdfUrl(url)
-    }
-  }
+import Pdf from '@/public'
 
   return (
     <Layout>
       <div>
-        <input type="file" accept=".pdf" onChange={handleFileUpload} />
-        {pdfUrl && (
-          <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe>
-        )}
+        /* <!-- <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe> --> */ 
+        <embed src="Pdf" width="800px" height="2100px" />
       </div>
     </Layout>
   )
