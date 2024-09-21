f827839a86707d97eade00427b7f0f829d5f5f1f
purpl3bot <mechi.farias@gmail.com>
2024-09-21 17:36:16 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -6,7 +6,9 @@ import Pdf from '@/public'
 
     <Layout>
       <div>
-        <embed src="Pdf" width="800px" height="2100px" />
+        <iframe src={Pdf} width="100%" height="600px" style={{ border: 'none' }}></iframe>  
+        /* <!-- <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe> --> */ 
+        /*  <embed src="Pdf" width="800px" height="2100px" /> */ 
       </div>
     </Layout>
 
