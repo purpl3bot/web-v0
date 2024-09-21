1f2d06e6f04732226d31d5628d5f4d0576ce9780
purpl3bot <mechi.farias@gmail.com>
2024-09-21 17:33:18 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,13 +4,10 @@ import { useState } from 'react'
 import Layout from '../../components/Layout'
 import Pdf from '@/public'
 
-export default function About() {
-  return (
     <Layout>
       <div>
         /* <!-- <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe> --> */ 
         <embed src="Pdf" width="800px" height="2100px" />
       </div>
     </Layout>
-  )
-}
+
