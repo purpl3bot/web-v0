3be3de37db41451301961c96cd6b81e7efad611d
purpl3bot <mechi.farias@gmail.com>
2024-09-21 17:46:57 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Layout from '../../components/Layout'
 
 const Pdf = [
-  { src: '/Pdf', width: 800, height: 600 },
+  { src: '/Pdf'},
   // Add more images with their respective dimensions
 ]
 
@@ -12,7 +12,7 @@ const Pdf = [
     <div>
           /* <!-- <iframe src={pdfUrl} width="100%" height="600px" style={{ border: 'none' }}></iframe> --> */ 
         /*  <embed src="Pdf" width="800px" height="2100px" /> */ 
-        <iframe src={Pdf} width="100%" height="600px" style={{ border: 'none' }}></iframe>  
+        <iframe src='Pdf' width="100%" height="600px" style={{ border: 'none' }}></iframe>  
     </div>
 </Layout>
 
