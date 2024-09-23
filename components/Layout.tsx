ff836158a85d3a3eb85cf888c91c3cd2b17dc3e2
purpl3bot <mechi.farias@gmail.com>
2024-09-23 14:29:46 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,16 +3,14 @@ import Link from 'next/link'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
-      <div className="fixed top-4 right-4 text-red-600 text-sm">
-        UPCOMING: 26-29 Sept
-      </div>
-      <div className="fixed top-4 right-4 text-red-600 text-sm"> 
-        2Chairs Group Show
+      <div className="flex flex-col top-4 right-4 text-red-600 text-sm">
+        <div>UPCOMING: 26-29 Sept</div>
+        <div>2Chairs Group Show</div>
       </div>
       <div className="flex">
         <nav className="w-64 p-8 flex flex-col justify-between h-screen fixed">
           <div>
-            <Link href="/" className="block mb-8 text-2xl font-astloch">
+            <Link href="/" className="block mb-8 text-3xl font-astloch font-bold">
               mercedes farias
             </Link>
             <a
