616caf58b3551980334ef2c59cce9c050bac2262
purpl3bot <mechi.farias@gmail.com>
2024-09-23 13:44:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${sourceSans3.variable} ${astloch.variable}`}>
-      <body className={sourceSans3.className}>{children}</body>
+      <body className={`${sourceSans3.variable} ${astloch.variable}`}>{children}</body>
     </html>
   )
 }
