3e2886783532c6b544bc8578d4798f6f76248740
purpl3bot <mechi.farias@gmail.com>
2024-09-24 13:09:20 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,13 +6,13 @@ const MobileMenu = dynamic(() => import('./MobileMenu'), { ssr: false })
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
-      <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 bg-slate-100 lg:hidden">
+      <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 bg-stone-100 lg:hidden">
         <MobileMenu />
         <Link href="/" className="text-2xl font-astloch font-bold">
           mercedes farias
         </Link>
       </div>
-      <div className="hidden lg:block fixed flex-col top-4 right-4 text-lime-100 text-sm selection:bg-slate-100 selection:text-black">
+      <div className="hidden lg:block fixed flex-col top-4 right-4 text-lime-100 text-sm selection:bg-stone-100 selection:text-black">
         <div>
         <span className="text-orange-700 selection:text-orange-700">UPCOMING: </span>
         <span className="text-black">26-29 Sept</span>
