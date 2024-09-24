818ab53fe6c46b0c4c0ae2c1654eda5cb2a50aae
purpl3bot <mechi.farias@gmail.com>
2024-09-24 13:02:25 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,9 +6,9 @@ const MobileMenu = dynamic(() => import('./MobileMenu'), { ssr: false })
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
-      <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 bg-white lg:hidden">
+      <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 bg-slate-100 lg:hidden">
         <MobileMenu />
-        <Link href="/" className="text-2xl font-astloch">
+        <Link href="/" className="text-2xl font-astloch font-bold">
           mercedes farias
         </Link>
       </div>
@@ -19,7 +19,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
         <div className="text-black">2Chairs Group Show</div>
       </div>
-      <div className="flex  pt-16 lg:pt-0">
+      <div className="flex lg:pt-0">
         <nav className="hidden lg:flex w-64 p-8 flex-col justify-between h-screen fixed">
           <div>
             <Link href="/" className="block mb-8 text-3xl font-astloch font-bold hover:bg-lime-100 hover:text-black">
@@ -51,7 +51,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </a>
           </div>
         </nav>
-        <main className="w-full lg:ml-64 p-4 lg:p-8">{children}</main>
+        <main className="w-full lg:ml-64 p-4 lg:p-8 pt-16 lg:pt-8">{children}</main>
       </div>
     </div>
   )
